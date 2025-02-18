1385a39784cf1fb4071b5af3247f99894b6e28fa
sachnun <sachnun12@gmail.com>
2025-02-18 12:35:55 +0000

    Add devcontainer configuration and Dependabot setup; update dev script and enhance error handling

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
 import { Hono } from 'hono'
+import { HTTPException } from 'hono/http-exception'
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Random Faker!')
 })
 
-// /random
+// route /random
 app.get('/random', (c) => {
   return c.json({ random: Math.random() })
+})
+
+app.notFound((c) => {
+  // 404 without body
+  return c.text('', 404)
+})
+
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    // Get the custom response
+    return err.getResponse()
+  }
+  // 500 without body
+  return c.text('', 500)
 })
 
 export default app
