501ce759d849923b0aa5ecccbfceab4f2c682a8f
sachnun <sachnun12@gmail.com>
2025-02-18 22:10:16 +0000

    Add postgres dependency and log visitor IP addresses in the API

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,36 @@
 import { OpenAPIHono } from '@hono/zod-openapi'
 import { HTTPException } from 'hono/http-exception'
 import { swaggerUI } from '@hono/swagger-ui'
+import { getConnInfo } from 'hono/cloudflare-workers'
+import postgres from "postgres";
 
 import randomRoutes from './routes/random'
 import accountRoutes from './routes/account'
 
-const app = new OpenAPIHono()
+const app = new OpenAPIHono<{
+  Bindings: {
+    DATABASE_URL: string
+  }
+}>()
 
 const description = `
 This is a random data generator API. It uses the \`faker-js\` library to generate random data.
 `
+
+app.use('*', async (c, next) => {
+  const info = getConnInfo(c)
+  const sql = postgres(c.env.DATABASE_URL);
+
+  if (info.remote.address) {
+    c.executionCtx.waitUntil(
+      sql`INSERT INTO visitors (ip) VALUES (${info.remote.address})`
+        .then(() => {
+          console.log('inserted')
+        })
+    );
+  }
+  return next()
+})
 
 app.doc('/openapi.json', {
   openapi: '3.0.0',
